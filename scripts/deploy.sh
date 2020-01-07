1560fc9d1dee8498223e00382e4326a90a6d0a1f
Henrik Steen <henrist@henrist.net>
2020-01-07 13:29:45 +0100

    Deploy to new server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -eu
 
->&2 echo "Automated deploy currently disabled while moving to new setup"
-exit
-
 if [ ! -f public/build/bundle.js ]; then
   echo "Missing build"
   exit 1
 fi
 
-rsync -avz --delete public/ root@foreningenbs.no:/var/www/aliases/smaabruket/public/
+rsync -avz --delete public/ root@coreos-2.foreningenbs.no:/data/web-1-www/aliases/smaabruket/public/
